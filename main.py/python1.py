f85d248c293e9a7d76a03b83562c0d588450f78e
buğra isi <123095386+Endbugra@users.noreply.github.com>
2025-02-17 13:15:57 +0300

    Add files via upload

---
+++ main.py/python1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_dataset(df_path):
+    df = pd.read_csv(df_path)
+    return df
+
+if __name__ == "__main__":
+    df = load_dataset("main.py/csv")
+    print(df.head())
